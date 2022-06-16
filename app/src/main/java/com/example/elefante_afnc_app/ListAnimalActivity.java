6730b80e9a6188ace0d04b901ac9a5445a944820
Nonichan <angelnonigo02@gmail.com>
2022-06-16 14:32:56 -0500

    Inicio del proyecto

---
+++ app/src/main/java/com/example/elefante_afnc_app/ListAnimalActivity.java
@@ -0,0 +1,14 @@
+package com.example.elefante_afnc_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListAnimalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_animal);
+    }
+}
