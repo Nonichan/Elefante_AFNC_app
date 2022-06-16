7f9fdc6e2b6ed2cf93827ddcfa49ee0c1d1cd9db
Nonichan <angelnonigo02@gmail.com>
2022-06-16 14:03:45 -0500

    Inicio del proyecto

---
+++ app/src/main/java/com/example/elefante_afnc_app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.elefante_afnc_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button_siguiente;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_siguiente = findViewById(R.id.btn_siguiente_main);
+        button_siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Bienvenido", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
